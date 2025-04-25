5c3fb355da0229c9d336ffd7b6d8279deffc271c
vparamon <paramonov.vdp@gmail.com>
2025-04-25 10:36:08 +0300

    page objects

--- ui/src/main/java/pages/LoginPage.java
+++ ui/src/main/java/pages/LoginPage.java
@@ -1,2 +1,29 @@
-package pages;public class LoginPage {
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+
+    public SelenideElement userNameField(){
+        return $("input[class*='ark-login-username']");
+    }
+
+    public SelenideElement passwordField(){
+        return $("input[class*='ark-login-password'");
+    }
+
+    public SelenideElement logInButton(){
+        return $("div.ark-login-button");
+    }
+
+    public void fillUsername(String username){
+        userNameField().sendKeys(username);
+    }
+
+    public void fillPassword(String password){
+        passwordField().sendKeys(password);
+    }
+
 }
